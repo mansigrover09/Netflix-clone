4c079da77186eb4d3a095576ed257b13a5cdf7db
Mansi <mansigrover2001@gmail.com>
2022-01-25 15:09:21 +0530

    initial commit

---
+++ src/Row.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+function Row({ title }) {
+  const [movies, setMovies] = useState( [] );
+
+
+
+  return (
+  <div>
+      <h2>{title}</h2>
+
+      {/* container -> posters */}
+  </div>
+  )
+}
+
+export default Row;
