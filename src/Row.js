c2572410e25e63aceffac4ee911d74b11cc8929d
Mansi <mansigrover2001@gmail.com>
2022-01-27 15:56:30 +0530

    More commits

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState( [] );
 
-
-
+  useEffect(() => {
+      //loads information 
+      //if [], run once when the row loads and don't run again
+      async function fetchData() {
+          const request = await axios.get(fetchUrl);
+          //https://api.themoviedb.org/3//discover/tv?api_key=${API_KEY}&with_network=123
+          setMovies(request.data.results);
+          return request;
+      }
+      fetchData();
+    }, [fetchUrl]);
   return (
   <div>
       <h2>{title}</h2>
